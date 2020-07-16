cd98b79880e8420d564acbc9c4765c106cad150e
Amit Min <amitmin@localhost.localdomain>
2020-07-16 10:37:18 -0400

    delete and recreates worksheets

--- V3V4_Cluster_Info/pullv3v4.py
+++ V3V4_Cluster_Info/pullv3v4.py
@@ -6,15 +6,16 @@ from time import sleep
 
 print('Place your ocm login token in "token.txt" - found at https://cloud.redhat.com/openshift/token')
 # build the ocm CLI login command
-login = 'ocm login --token="'
+login = 'ocm login --token '
 with open('token.txt', 'r') as f:
-    login = login + f.read() + '"'
+    login = login + f.read()
+
 
 while True:
     # Login to ocm with token.txt and run the bash scripts to generate csv's.
     subprocess.run(login, shell=True)
-    subprocess.call(['./pullv4data.sh'])
-    subprocess.call(['./pullv3data.sh'])
+    # subprocess.call(['./pullv4data.sh'])
+    # subprocess.call(['./pullv3data.sh'])
 
     # authorize google sheet api with credentials and import csv to sheets
     scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
@@ -22,24 +23,45 @@ while True:
     client = gspread.authorize(creds)
 
     # V4 Cluster worksheet
-    sheet = client.open_by_key('1ry_tos2ZityB4futWmUTNmXN5q-NnZwIF_BqNv9n8E8').worksheet("V4Cluster")
+    baseSheet = client.open_by_key('1ry_tos2ZityB4futWmUTNmXN5q-NnZwIF_BqNv9n8E8')
+    
     with open('ocm_ocp4.csv', newline='') as csvfile:
         data = list(csv.reader(csvfile))
-    sheet.clear()    # clear entire sheet before inserting new sheet
+    try:
+        sheet = client.open_by_key('1ry_tos2ZityB4futWmUTNmXN5q-NnZwIF_BqNv9n8E8').worksheet("V4Cluster")
+        baseSheet.del_worksheet(sheet)
+    except:
+        pass
+    baseSheet.add_worksheet("V4Cluster",1,1)
+    sheet = client.open_by_key('1ry_tos2ZityB4futWmUTNmXN5q-NnZwIF_BqNv9n8E8').worksheet("V4Cluster")
     sheet.insert_rows(data, row=1)
+
 
     # V3 Cluster worksheet
-    sheet = client.open_by_key('1ry_tos2ZityB4futWmUTNmXN5q-NnZwIF_BqNv9n8E8').worksheet("V3Cluster")
+    baseSheet = client.open_by_key('1ry_tos2ZityB4futWmUTNmXN5q-NnZwIF_BqNv9n8E8')
     with open('V3_Cluster_Info.csv', newline='') as csvfile:
         data = list(csv.reader(csvfile))
-    sheet.clear()
+    try:
+        sheet = client.open_by_key('1ry_tos2ZityB4futWmUTNmXN5q-NnZwIF_BqNv9n8E8').worksheet("V3Cluster")
+        baseSheet.del_worksheet(sheet)
+    except:
+        pass
+    baseSheet.add_worksheet("V3Cluster",1,1)
+    sheet = client.open_by_key('1ry_tos2ZityB4futWmUTNmXN5q-NnZwIF_BqNv9n8E8').worksheet("V3Cluster")
     sheet.insert_rows(data, row=1)
 
+
     # V3 Nodes worksheet
-    sheet = client.open_by_key('1ry_tos2ZityB4futWmUTNmXN5q-NnZwIF_BqNv9n8E8').worksheet("V3Nodes")
+    baseSheet = client.open_by_key('1ry_tos2ZityB4futWmUTNmXN5q-NnZwIF_BqNv9n8E8')
     with open('V3_Node_Info.csv', newline='') as csvfile:
         data = list(csv.reader(csvfile))
-    sheet.clear()
+    try:
+        sheet = client.open_by_key('1ry_tos2ZityB4futWmUTNmXN5q-NnZwIF_BqNv9n8E8').worksheet("V3Nodes")
+        baseSheet.del_worksheet(sheet)
+    except:
+        pass
+    baseSheet.add_worksheet("V3Nodes",1,1)
+    sheet = client.open_by_key('1ry_tos2ZityB4futWmUTNmXN5q-NnZwIF_BqNv9n8E8').worksheet("V3Nodes")
     sheet.insert_rows(data, row=1)
 
     print("v3v4: sleeping for 1 hour")
